95dd96ceef2adf2c6cb1d639aa5b6cacced4ad8c
Alex Vear <axvr@users.noreply.github.com>
2018-03-24 14:47:19 +0000

    First version of static HTML page generator

---
+++ static-gen.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Simple script to generate the static HTML pages for my website
+
+
+# TODO RSS feed construction (and where to put the link)
+
+for FILE in $(find . -name '*-content.html' -o -name '*.md'); do
+
+    if [ $FILE == "./README.md" ]; then
+        continue
+    fi
+
+    # Tags could be comments, and the title could be a <h1> tag
+    TITLE=$(sed -n 's/^###TITLE###:\(.*\)$/\1/p' "$FILE")
+
+    OUT_FILE=$(echo "$FILE" | sed -n 's/\(\.md\|-content\.html\)$/\.html/p')
+    cp "template.html" "$OUT_FILE"
+
+    sed -i "s/###TITLE###/$TITLE/" "$OUT_FILE"
+
+    # TODO If a markdown file, convert it first (after removing "###TITLE###")
+
+    sed -i -e "/###CONTENT###/r $FILE" "$OUT_FILE"
+    sed -i '/\(^###TITLE###:\|###CONTENT###\)/d' "$OUT_FILE"
+
+    echo "$FILE --> $OUT_FILE"
+
+done
+
