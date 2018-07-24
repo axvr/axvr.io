395451422457f54db206a19a12e1f71de6f99d64
Alex Vear <axvr@users.noreply.github.com>
2018-07-24 21:22:21 +0100

    Remove the inner CSS grid

--- scripts/blog-feed.sh
+++ scripts/blog-feed.sh
@@ -17,10 +17,11 @@ for ARTICLE in $(find ./blog/ -name '*.content.html' | sort -r | sed 10q); do
     DESC_END=$(grep -nm 1 "</p>"  "$ARTICLE" | sed -n 's/^\([0-9]\+\):.*$/\1/p')
 
     {
-        printf "\\n<div class=\"item\">\\n"
+        # printf "\\n<div class=\"item\">\\n"
         printf "<h2><a href=\"%s\">%s</a></h2>\\n" "$URL" "$TITLE"
         printf "%s\\n" "$(sed -n "${DESC_START},${DESC_END}p" "$ARTICLE")"
-        printf "</div>\\n"
+        printf "<br>\\n"
+        # printf "</div>\\n"
     } >> "$FEED_PAGE"
 
     printf "%s --> %s\\n" "$ARTICLE" "$FEED_PAGE"
