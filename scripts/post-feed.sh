8e7fe24c6bd9a252f5b18c63734303507123145e
Alex Vear <av@axvr.io>
2019-04-03 21:17:06 +0100

    Fix post generation

--- scripts/post-feed.sh
+++ scripts/post-feed.sh
@@ -13,7 +13,7 @@ EOF
 # Itterate over all articles in chronological order and take the top 15.
 for article in $(find ./posts/*/ -name '*.html.tmp' | sort -r | sed 15q); do
 
-    title=$(sed -n 's/<h1>\(.*\)<\/h1>/\1/p' "$article")
+    title=$(sed -n 's/<h1.*\?>\(.*\)<\/h1>/\1/p' "$article")
     url=$(printf "%s" "$article" | sed -n 's/\/[^/]\+$//p' | sed -n 's/^\.//p')
 
     # Find the first paragraph, and grab the line numbers. This paragraph will
